1ea04dd3fc9ea15a044d396de8ed63db0527e3fe
tohirovmuhammadsodiq <220481@centralasian.uz>
2026-02-24 16:27:56 +0500

    chore: remove global authentication middleware

--- middleware/auth.global.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('Global auth middleware', to, from);
-  const isLoggedIn = !!useCookie('access_token').value;
-
-  if (!isLoggedIn && to.path !== '/auth') {
-    return navigateTo('/auth');
-  }
-});
